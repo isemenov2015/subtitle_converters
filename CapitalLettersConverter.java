7ecfa059123bd0b0d9c9a1cc0cbba151a069fb3d
Ilya Semenov <semenovio@gmail.com>
2017-10-24 15:04:38 +0300

    Update CapitalLettersConverter.java

--- CapitalLettersConverter.java
+++ CapitalLettersConverter.java
@@ -1,9 +1,9 @@
 /**
 * Write a description of XML_converter here.
 * 
- * @author Ilya Semenov 
- * @version 1.0
-* Capital letters converter after </p> and </br> tags for Netflix subtitles format files
+ * @author (your name) 
+ * @version (a version number or a date)
+* Converter from XML to Netflix subtitles format
 */
 
 import edu.duke.FileResource;
@@ -26,15 +26,20 @@ public class CapitalLettersConverter {
         FileResource input = new FileResource();
         try (Writer writer = new BufferedWriter(new OutputStreamWriter(
                   new FileOutputStream("g:/output.dfxp"), "utf-8"))) {
+                        boolean headerDone = false;
+                        boolean flagLowerCase = false;
                         System.out.println("Writing header");
                         for (String str : input.lines()) {
-                            writer.write(str + System.getProperty("line.separator"));
-                            if (str.indexOf("<div>") > 0)
-                                break;
-                        }
-                        System.out.println("Writing body");
-                        boolean flagLowerCase = false;
-                        for (String str : input.lines()) {
+                          if (!headerDone) {
+                                //System.out.println(str);
+                                writer.write(str + System.getProperty("line.separator"));
+                                if (str.indexOf("<div>") > 0) {
+                                    headerDone = true;
+                                    System.out.println("Writing body");
+                                }
+                                continue;
+                          }
+                          //System.out.println(str);
                           int indexBr = str.indexOf("<br/>");
                           if (indexBr > 0) {
                               //System.out.println("String found:" + str);
@@ -45,6 +50,12 @@ public class CapitalLettersConverter {
                                   newStr.setCharAt(indexBr + 5, chr);
                                   str = newStr.toString();
                                 }
+                              else  {
+                                  StringBuilder newStr = new StringBuilder(str);
+                                  char chr = Character.toUpperCase(newStr.charAt(indexBr + 5));
+                                  newStr.setCharAt(indexBr + 5, chr);
+                                  str = newStr.toString();
+                              }
                             }
                           if (flagLowerCase) {
                               int indexFirstChar = str.indexOf("\">");
@@ -55,6 +66,18 @@ public class CapitalLettersConverter {
                                   str = newStr.toString();
                                 }
                             }
+                          else {
+                              int indexFirstChar = str.indexOf("\">");
+                              if (indexFirstChar > 0) {
+                                  StringBuilder newStr = new StringBuilder(str);
+                                  //System.out.println(str);
+                                  char chr = Character.toUpperCase(newStr.charAt(indexFirstChar + 2));
+                                  newStr.setCharAt(indexFirstChar + 2, chr);
+                                  str = newStr.toString();
+                                  flagLowerCase = false;
+                                }
+                          }
+                         
                           int lastIndex = str.lastIndexOf("</span></p>");
                           if (lastIndex < 0) {
                               lastIndex = str.lastIndexOf("</p>");
